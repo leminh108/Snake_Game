87f16a8c5c844dfd4f3bd4a29649631c2aba2413
leminh108 <leminh10897@gmail.com>
2025-09-09 15:49:43 +0700

    commt fire base

--- firebase-config.js
+++ firebase-config.js
@@ -30,22 +30,6 @@ const firebaseConfig = {
   appId: getEnvVar("VITE_FIREBASE_APP_ID", "")
 };
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCmEGJcbWByoqLtlMGi1jGvQ6Vniq8CBYo",
-//   authDomain: "snake-game-4b661.firebaseapp.com",
-//   projectId: "snake-game-4b661",
-//   storageBucket: "snake-game-4b661.firebasestorage.app",
-//   messagingSenderId: "290253437440",
-//   appId: "1:290253437440:web:ff9664917a32b497f4b6e5",
-//   measurementId: "G-JLPQ7V7PXB"
-// };
-
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 
